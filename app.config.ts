a49f5f932a9b01f6ef4b27721dc48269af60597a
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-03-25 22:40:21 +0700

    add mapview

---
+++ app.config.ts
@@ -0,0 +1,42 @@
+import type { ConfigContext, ExpoConfig } from 'expo/config'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// React Native Maps: https://docs.expo.dev/versions/latest/sdk/map-view/
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'wheel-go-rn',
+  slug: 'wheel-go-rn',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.nakama.wheelgo',
+    config: {
+      googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+    },
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    config: {
+      googleMaps: {
+        apiKey: GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
+  scheme: 'wheelgo',
+})
