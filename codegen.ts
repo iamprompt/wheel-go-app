350da655d8ad217d4f684564c17cb0943dc79bde
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-03-26 12:21:15 +0700

    add env codegen

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,12 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const WHEELGO_CMS_API = process.env.WHEELGO_CMS_API
 
 const config: CodegenConfig = {
-  schema: `https://cms.wheelgo.iamprompt.me/api/graphql`,
+  schema: `${WHEELGO_CMS_API}/graphql`,
   documents: ['graphql/**/*.{ts,tsx}'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
