0194bb094e2f0b68792f489c91c9282ffabc92e0
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-03-29 23:54:09 +0700

    add policy text

--- src/app/policy.tsx
+++ src/app/policy.tsx
@@ -1,8 +1,8 @@
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { ScrollView, Text, View } from 'react-native'
+import { Policy } from '~/const/policy'
 import { GlobalStyle } from '~/styles'
-import COLORS from '~/styles/colors'
 import FONTS from '~/styles/fonts'
 
 const Page = () => {
@@ -33,15 +33,7 @@ const Page = () => {
       >
         Privacy Policy
       </Text>
-      <Text
-        style={{
-          fontFamily: FONTS.LSTH_BOLD,
-          fontSize: 16,
-          color: COLORS.magenta[500],
-        }}
-      >
-        Effective Date: 1 January 2023
-      </Text>
+
       <View
         style={{
           marginTop: 12,
@@ -53,33 +45,7 @@ const Page = () => {
             fontSize: 12,
           }}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-          ea commodo consequat. Duis aute irure dolor in reprehenderit in
-          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia
-          deserunt mollit anim id est laborum.
+          {Policy.en}
         </Text>
       </View>
     </ScrollView>
