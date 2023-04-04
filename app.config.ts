3d757f821e3596623cf2ddae8f996bfb4aadd6e1
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-04-04 18:27:06 +0700

    update build number

--- app.config.ts
+++ app.config.ts
@@ -20,7 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   assetBundlePatterns: ['**/*'],
   ios: {
-    buildNumber: '5',
+    buildNumber: '6',
     supportsTablet: true,
     bundleIdentifier: 'com.nakama.wheelgo',
     config: {
