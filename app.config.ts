e74111cf2ade6634f98d0ab3365f3f80a2290310
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-04-10 13:44:28 +0700

    test ci

--- app.config.ts
+++ app.config.ts
@@ -20,7 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   assetBundlePatterns: ['**/*'],
   ios: {
-    buildNumber: '17',
+    buildNumber: '18',
     supportsTablet: true,
     bundleIdentifier: 'com.nakama.wheelgo',
     config: {
